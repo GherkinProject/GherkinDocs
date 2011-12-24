105fea1094a916dc959a26b5cba1d8c2d2971f5d
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2011-12-24 18:07:38 +0100

    config more detailed and with more default values

--- config.py
+++ config.py
@@ -2,4 +2,9 @@
 
 #list of working extensions :
 defaultFileExt = {".mp3":"MP3", ".ogg":"OGG", ".aif":"AIF"}
+
+#text Database location
 defaultDbLocation = "./db.xml"
+
+#Useful tag kept
+defaultTagKept = {"artist", "album", "title", "date", "tracknumber", "genre"}
