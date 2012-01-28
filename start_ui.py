d2931c89fbad78cb9860cf7cb4a883b5175d1ff0
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2012-01-28 10:16:48 +0100

    Adding folders to complete previous commit

---
+++ start_ui.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+#adding path of audio player to the lib
+import sys
+if 'ui/' not in sys.path:
+    sys.path.append('ui/')
+
+#ui is calling the database
+if 'db-tools/' not in sys.path:
+    sys.path.append('db-tools/')
+
+#running server
+import runIG
