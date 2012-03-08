089965212521067d37074fc3dd76a495add47f88
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2012-03-08 17:48:16 +0100

    Only a few buttons left

--- ui/runIG.py
+++ ui/runIG.py
@@ -11,6 +11,9 @@ from load_db import *
 #configuration constant
 import config
 
+#time for progress bar
+import time
+
 #client lib for calling server
 import xmlrpclib
 
@@ -25,22 +28,23 @@ class MyForm(QtGui.QMainWindow):
           
         #they come together
         self.playlist = self.server.get_playlist()
+        self.pointeur = self.server.get_point()
         
         #saving artists and songs displayed
         #getting the lib from the xml file
         if config.serverName == "localhost":
             (self.artists, self.albums, self.songs) = get_lib()
 	
-        #display artists and albums at launch
-        self.display_all()
+        #display artists and albums at launch, if server is playing, display current infos
+        if self.playlist != []:
+            self.update_artists()
+            if self.pointeur != -1:
+                self.display_name()
+                if self.server.is_playing():
+                    self.runSong()
+        else:
+            self.display_all()
 
-        #self.server.load()
-
-        #loading song into the server
-        #TO BE CHANGE ! id = 0 may not exist !
-        #self.playlist.append(self.songs.keys()[0])
-        #self.server.load(self.songs[self.playlist[self.pointeur]]['location'])
-        
         self.update_tracks()
 
         #signal received, functions called
@@ -58,25 +62,12 @@ class MyForm(QtGui.QMainWindow):
         QtCore.QObject.connect(self.ui.LookingFor, QtCore.SIGNAL("textEdited(QString)"), self.call_search)
 #        QtCore.QObject.connect(self.ui.verticalSlider, QtCore.SIGNAL("valueChanged(int)"), self.call_volume )	
 
-    def add_entry(self):
-        self.ui.lineEdit.selectAll()
-        self.ui.lineEdit.cut()
-        self.ui.textEdit.append("")
-        self.ui.textEdit.paste()
-    
-    def load(self):
-        #terminate process, if existing
-        try:
-            self.song_play.terminate()
-        except:
-            pass
-        try:
-            self.ui.LookingForNoTouch.setText(self.songs[self.playlist[self.pointeur]]["title"]+" - "+ self.songs[self.playlist[self.pointeur]]['artist'])
-        except:
-            self.ui.LookingForNoTouch.setText("Unknown")
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+#Interface with server
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
 
-        self.server.load()
-        
     def call_play_pause(self):
         self.server.play_pause()
 
@@ -94,155 +85,32 @@ class MyForm(QtGui.QMainWindow):
         """When a song is doubleclicked on in the playlist"""
         #we have the id of the song clicked on
         idSong = int(QtWidget.text(4))
-
-        #deselect the track in the ui
-        self.ui.AudioTrack.topLevelItem(self.pointeur).setSelected(False)
-        
         self.pointeur = 0
         #looking for the selected track in the playlist
         while self.playlist[self.pointeur] != idSong:
             self.pointeur += 1
-
-        #loading and playing
+        
+        self.deselect()
         self.server.change(self.pointeur)
-        
-        #needs graphical changes
-        self.load()
-        self.call_play_pause()
-    
-    def display_all(self):
-        """display all albums and artists"""
-        #cleaning the lists
-        self.ui.Artist.clear()
-        self.ui.Album.clear()
-        self.ui.AudioTrack.clear()
-        
-        #sorting the artists and albums by name for it to be smarter, and display
-        artists = self.artists.keys()
-        artists.sort()
-        self.selectedSongs = set()
-        for artist in artists:
-            self.ui.addArtist(artist)
-            albums = list(self.artists[artist])
-            albums.sort()
-            for album in albums:
-                self.ui.addAlbum(album)
-                for idTrack in self.albums[album]:
-                    self.selectedSongs.add(idTrack)
-        
-        #then create a playlist from this set
-        self.playlist = make_neighbors(self.songs, self.selectedSongs)
-        self.server.set_playlist(self.playlist)
-        self.pointeur = 0
-        
-        #and update the ui then
-        self.update_albums()
- 
-    def call_play_albums(self, QtWidget):
-        self.call_albums(QtWidget)
-        self.load()
-        self.call_play_pause()
-
-    def call_play_tracks(self, QtWidget):
-        self.call_tracks(QtWidget)
-        self.load()
-        self.call_play_pause()
-    
-    def call_albums(self, QtWidget, val = 0):
-        """When an artist is clicked on..."""
-        self.selectedArtist = str(QtWidget.text(0))
-        self.selectedSongs = set()
-        
-        #adding all the songs to the set
-        for album in self.artists[self.selectedArtist]:
-            for idTrack in self.albums[album]:
-                self.selectedSongs.add(idTrack)
-        
-        #then create a playlist from this set
-        self.playlist = make_neighbors(self.songs, self.selectedSongs)
-        self.server.set_playlist(self.playlist)
-        self.pointeur = 0
-        
-        #and update the ui then
-        self.update_albums()
-    
-    def update_albums(self):
-        #removing elements from the album tree
-        self.ui.Album.clear()
-        self.ui.AudioTrack.clear()
-        
-        #add them in the order of the playlist, that is to say, alphabetical order for albums
-        self.displayedAlbums = set()
-        for idTrack in self.playlist:
-            #adding an album if he hasn't already been displayed
-            if self.songs[idTrack]["album"] not in self.displayedAlbums:
-                self.displayedAlbums.add(self.songs[idTrack]["album"])
-                self.ui.addAlbum(self.songs[idTrack]["album"])
-            self.ui.addTrack(self.songs[idTrack])
-    
-    def update_artists(self):
-        #removing elements from the artist tree
-        self.ui.Artist.clear()
-        self.ui.Album.clear()
-        self.ui.AudioTrack.clear()
-        
-        #add them in the ordre of the playlist => alphabetical order for artists
-        self.displayedArtists = set()
-        self.displayedAlbums = set()
-        for idTrack in self.playlist:
-            #one should be added if not yet displayed
-            if self.songs[idTrack]["artist"] not in self.displayedArtists:
-                self.displayedArtists.add(self.songs[idTrack]["artist"])
-                self.ui.addArtist(self.songs[idTrack]["artist"])
-            if self.songs[idTrack]["album"] not in self.displayedAlbums:
-                self.displayedAlbums.add(self.songs[idTrack]["album"])
-                self.ui.addAlbum(self.songs[idTrack]["album"])
-            self.ui.addTrack(self.songs[idTrack])
-
-    def call_tracks(self, QtWidget, val = 0):
-        """When an album is doubleclicked on"""
-        self.selectedAlbum = str(QtWidget.text(0))
-        self.selectedSongs = self.albums[self.selectedAlbum] 
-        
-        #making the playlist from the set of songs
-        self.playlist = make_neighbors(self.songs, self.selectedSongs)
-        self.server.set_playlist(self.playlist)
-        self.pointeur = 0
-    
-        self.update_tracks()
-
-    def update_tracks(self):
-        #removing elements from the album tree
-        self.ui.AudioTrack.clear()
-
-        for idTrack in self.playlist:
-		self.ui.addTrack(self.songs[idTrack])
-    
- 
+        self.apply_changes()
 
     def call_next(self):
-        """The function return True if it has found a new song to play, False either"""
-        self.server.set_next()
-        self.ui.AudioTrack.topLevelItem(self.pointeur).setSelected(False)
-        self.server.play_pause()
+        """The function return True if it has found a new song to play, False either""" 
+        self.deselect()
+        self.server.next()
+        self.apply_changes()
 
     def call_prev(self):
         """When previous button clicked on, convention : go to the end if at the first"""
-        self.ui.AudioTrack.topLevelItem(self.pointeur).setSelected(False)
-        
-        #if we are not at the first element, no problem
-        if self.pointeur > 0:
-            self.pointeur-=1
-            self.load()
-        else:
-            self.pointeur = len(self.playlist)-1
-            self.load()
-        
-        #erasing the lasts elements in those mode taking into account the user didn't like the music proposed
-        if self.mode == playlist or self.mode == random:
-            self.playlist.pop(-1)
-            self.update_tracks()
-        
+        self.deselect()
+        self.server.prev()
+        self.apply_changes()
+
+    def apply_changes(self):
+        self.pointeur = self.server.get_point()
+        self.playlist = self.server.get_playlist()
+       
+        self.display_name()
         self.call_play_pause()
 
     def call_random(self):
@@ -319,8 +187,154 @@ class MyForm(QtGui.QMainWindow):
     def call_volume(self, int):
         self.server.set_volume(int * 10 / (self.ui.verticalSlider.maximum()-self.ui.verticalSlider.minimum()))
 
-   
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+#UI methods
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
 
+#----------------------------
+#modifying constants
+#----------------------------
+
+    def set_playlist(self, playlist):
+        self.server.set_playlist(playlist)
+        self.playlist = playlist
+
+    def set_pointeur(self, pointeur):
+        self.server.set_point(pointeur)
+        self.pointeur = pointeur
+
+#----------------------------
+#only display methods
+#----------------------------
+
+    def deselect(self):
+       if self.pointeur != -1:
+            self.ui.AudioTrack.topLevelItem(self.pointeur).setSelected(False)
+    
+    def add_entry(self):
+        self.ui.lineEdit.selectAll()
+        self.ui.lineEdit.cut()
+        self.ui.textEdit.append("")
+        self.ui.textEdit.paste()
+    
+    def display_name(self):
+        #terminate process, if existing
+        try:
+            self.song_play.terminate()
+        except:
+            pass
+        try:
+            self.ui.LookingForNoTouch.setText(self.songs[self.playlist[self.pointeur]]["title"]+" - "+ self.songs[self.playlist[self.pointeur]]['artist'])
+        except:
+            self.ui.LookingForNoTouch.setText("Unknown")
+
+    def display_all(self):
+        """display all albums and artists"""
+        #cleaning the lists
+        self.ui.Artist.clear()
+        self.ui.Album.clear()
+        self.ui.AudioTrack.clear()
+        
+        #sorting the artists and albums by name for it to be smarter, and display
+        artists = self.artists.keys()
+        artists.sort()
+        self.selectedSongs = set()
+        for artist in artists:
+            self.ui.addArtist(artist)
+            albums = list(self.artists[artist])
+            albums.sort()
+            for album in albums:
+                self.ui.addAlbum(album)
+                for idTrack in self.albums[album]:
+                    self.selectedSongs.add(idTrack)
+        
+        #then create a playlist from this set
+        self.set_playlist(make_neighbors(self.songs, self.selectedSongs))
+        self.set_pointeur(0)
+        
+        #and update the ui then
+        self.update_albums()
+ 
+    def call_play_albums(self, QtWidget):
+        self.call_albums(QtWidget)
+        self.display_name()
+        self.call_play_pause()
+
+    def call_play_tracks(self, QtWidget):
+        self.call_tracks(QtWidget)
+        self.display_name()
+        self.call_play_pause()
+    
+    def call_albums(self, QtWidget, val = 0):
+        """When an artist is clicked on..."""
+        self.selectedArtist = str(QtWidget.text(0))
+        self.selectedSongs = set()
+        
+        #adding all the songs to the set
+        for album in self.artists[self.selectedArtist]:
+            for idTrack in self.albums[album]:
+                self.selectedSongs.add(idTrack)
+        
+        #then create a playlist from this set
+        self.set_playlist(make_neighbors(self.songs, self.selectedSongs))
+        self.set_pointeur(0)
+        
+        #and update the ui then
+        self.update_albums()
+    
+    def update_albums(self):
+        #removing elements from the album tree
+        self.ui.Album.clear()
+        self.ui.AudioTrack.clear()
+        
+        #add them in the order of the playlist, that is to say, alphabetical order for albums
+        self.displayedAlbums = set()
+        for idTrack in self.playlist:
+            #adding an album if he hasn't already been displayed
+            if self.songs[idTrack]["album"] not in self.displayedAlbums:
+                self.displayedAlbums.add(self.songs[idTrack]["album"])
+                self.ui.addAlbum(self.songs[idTrack]["album"])
+            self.ui.addTrack(self.songs[idTrack])
+    
+    def update_artists(self):
+        #removing elements from the artist tree
+        self.ui.Artist.clear()
+        self.ui.Album.clear()
+        self.ui.AudioTrack.clear()
+        
+        #add them in the ordre of the playlist => alphabetical order for artists
+        self.displayedArtists = set()
+        self.displayedAlbums = set()
+        for idTrack in self.playlist:
+            #one should be added if not yet displayed
+            if self.songs[idTrack]["artist"] not in self.displayedArtists:
+                self.displayedArtists.add(self.songs[idTrack]["artist"])
+                self.ui.addArtist(self.songs[idTrack]["artist"])
+            if self.songs[idTrack]["album"] not in self.displayedAlbums:
+                self.displayedAlbums.add(self.songs[idTrack]["album"])
+                self.ui.addAlbum(self.songs[idTrack]["album"])
+            self.ui.addTrack(self.songs[idTrack])
+
+    def call_tracks(self, QtWidget, val = 0):
+        """When an album is doubleclicked on"""
+        self.selectedAlbum = str(QtWidget.text(0))
+        self.selectedSongs = self.albums[self.selectedAlbum] 
+        
+        #making the playlist from the set of songs
+        self.set_playlist(make_neighbors(self.songs, self.selectedSongs))
+        self.set_pointeur(0)
+    
+        self.update_tracks()
+
+    def update_tracks(self):
+        #removing elements from the album tree
+        self.ui.AudioTrack.clear()
+
+        for idTrack in self.playlist:
+		self.ui.addTrack(self.songs[idTrack])
+    
     def iconChange(self):
         u = self.server.is_playing()
         if u:
@@ -336,8 +350,7 @@ class MyForm(QtGui.QMainWindow):
   
     def runSong(self):
         self.song_play = Song()
-        self.connect(self.song_play, QtCore.SIGNAL("progressUpdated"),
-        self.updateSongProgress2)
+        self.connect(self.song_play, QtCore.SIGNAL("progressUpdated"), self.updateSongProgress2)
         self.song_play.start()
 
     def updateSongProgress(self, min, max, progress):
@@ -349,24 +362,25 @@ class MyForm(QtGui.QMainWindow):
     def updateSongProgress2(self):
         self.ui.SongBar.setMinimum(0)
         try:
-	    u = self.server.get_position()
-	    v = self.server.get_duration()
+            u = self.server.get_position()
+            v = self.server.get_duration()
             self.ui.SongBar.setMaximum(v)
             self.ui.SongBar.setValue(u)
-	    
-	    time = ""
-	    if u % 60 < 10 and v % 60 < 10:
-		time = str(u // 60) + " : " + "0"+str(u % 60) + " / " + str(v // 60) + " : " + "0" + str(v % 60)
-	    elif u % 60 >= 10 and v % 60 < 10:
-		time = str(u // 60) + " : " + str(u % 60) + " / " + str(v // 60) + " : " + "0" + str(v % 60)
-	    elif u % 60 < 10 and v % 60 >= 10:
-		time = str(u // 60) + " : " + "0"+str(u % 60) + " / " + str(v // 60) + " : "  + str(v % 60)
-	    else:
-		time = str(u // 60) + " : " + str(u % 60) + " / " + str(v // 60) + " : " +  str(v % 60)
-	           
-	    self.ui.SongBar.setFormat(time)
+
+            time = ""
+            if u % 60 < 10 and v % 60 < 10:
+                time = str(u // 60) + " : " + "0"+str(u % 60) + " / " + str(v // 60) + " : " + "0" + str(v % 60)
+            elif u % 60 >= 10 and v % 60 < 10:
+                time = str(u // 60) + " : " + str(u % 60) + " / " + str(v // 60) + " : " + "0" + str(v % 60)
+            elif u % 60 < 10 and v % 60 >= 10:
+                time = str(u // 60) + " : " + "0"+str(u % 60) + " / " + str(v // 60) + " : "  + str(v % 60)
+            else:
+                time = str(u // 60) + " : " + str(u % 60) + " / " + str(v // 60) + " : " +  str(v % 60)
+                   
+            self.ui.SongBar.setFormat(time)
         except:
             pass
+
         self.ui.SongBar.repaint()
         
         try:
@@ -378,6 +392,7 @@ class MyForm(QtGui.QMainWindow):
                     self.call_next()
         except:
             pass
+
     def call_search(self, QString):
         self.selectSongs = set()
         for artist in self.artists:
@@ -390,9 +405,8 @@ class MyForm(QtGui.QMainWindow):
                     except:
                         pass
         #then create a playlist from this set
-        self.playlist = make_neighbors(self.songs, self.selectSongs)
-        self.server.set_playlist(self.playlist)
-        self.pointeur = 0
+        self.set_playlist(make_neighbors(self.songs, self.selectSongs))
+        self.set_pointeur(0)
         
         #and update the ui then
         self.update_artists()
@@ -406,9 +420,9 @@ class Song(QtCore.QThread):
         self.max = 100000
         self.progress = 0
     def run(self):
-	for self.progress in range(self.min, self.max):
+        for self.progress in range(self.min, self.max):
             self.emit(QtCore.SIGNAL("progressUpdated"))
-            time.sleep(0.2)  
+            time.sleep(1)  
      
         
 
