a14b5e6ba9cbcb3a10e4ebd27010b82c18448d9a
Gautier Dakin <gautier.dakin@gmail.com>
2012-06-11 12:33:36 +0200

    Bouh, c'est toi le rapporteur

--- learning/clustering.R
+++ learning/clustering.R
@@ -9,7 +9,7 @@ mydata <- mydata[1:g-1,]
 
 
 
-nbre_cluster <- 15
+nbre_cluster <- 20
 # Normalisation des données
 for(i in 1:length(mydata))
     mydata[,i]=mydata[,i]/max(mydata[,i])
@@ -19,13 +19,22 @@ for(i in 1:length(mydata))
 #Clustering classique avec les k-means.
 KMEANS <- kmeans(mydata, nbre_cluster)
 
+plotcluster <-function(Data, Algo,nbre_cluster){
 
-plot(mydata[,1:2], col = KMEANS$cluster)
-points(KMEANS$centers, col = 1:nbre_cluster, pch = 8)
+plot(Data[,1:2], col = Algo$cluster)
+points(Algo$centers, col = 1:nbre_cluster, pch = 8)
+return(0)}
 
+plotclustering <- function(Data, Algo, nbre_cluster){
 
+plot(Data[,1:2], col = Algo$clustering)
+points(Algo$medoids, col = 1:nbre_cluster, pch = 8)
+return(0)
+}
+print('plotting KMEANS')
+plotcluster(mydata, KMEANS, nbre_cluster)
 
-mydata <-mydata[1:200,]
+#mydata <-mydata[1:1000,]
 # Hierarchical clustering
 #mydist <- dist(mydata)
 #HCLUST <- hclust(mydist)
@@ -48,8 +57,11 @@ mydata <-mydata[1:200,]
 
 FANNY <- fanny(mydata, nbre_cluster, diss=FALSE, memb.exp = 1.2,metric = c("euclidean", "manhattan", "SqEuclidean"),stand = FALSE, iniMem.p = NULL, cluster.only = FALSE)
 
+print('plotting FANNY')
+plotclustering(mydata, FANNY, nbre_cluster)
 # Partionning around Medoids
 
 PAM <-pam(mydata, nbre_cluster, FALSE, metric = "euclidean",medoids = NULL, stand = FALSE, cluster.only = FALSE)
 
-
+print('plotting PAM')
+plotclustering(mydata, PAM, nbre_cluster)
